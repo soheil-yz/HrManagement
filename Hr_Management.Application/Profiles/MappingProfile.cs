5c53a37570b80a660b886ec705dc1aa820e1e8d3
soheil yazdi1 <soheilyazdanii1382@gmail.com>
2023-12-10 14:03:38 +0330

    Put AutoMapper in Project And Set Using

---
+++ Hr_Management.Application/Profiles/MappingProfile.cs
@@ -0,0 +1,12 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Hr_Management.Application.Profiles
+{
+    public class MappingProfile : Profile
+    {
+        
+    }
+}
