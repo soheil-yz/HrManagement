48cea77e869287bff29ab2024dd9b4b24b94f7a3
soheil yazdi1 <soheilyazdanii1382@gmail.com>
2024-02-19 21:16:08 +0330

    Create Model For Email And Interface

---
+++ Hr_Management.Application/Contracts/Infrastructure/IEmailSender.cs
@@ -0,0 +1,13 @@
+﻿using Hr_Management.Application.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hr_Management.Application.Contracts.Infrastructure
+{
+    public interface IEmailSender
+    {
+        Task<bool> SenderEmail(Email email);
+    }
+}
---
+++ Hr_Management.Application/Models/EmailSetting.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Hr_Management.Application.Models
+{
+    public class EmailSetting
+    {
+        public string ApiKey { get; set; }
+        public string FromAddress { get; set; }
+        public string FromName { get; set; }
+    }
+}
